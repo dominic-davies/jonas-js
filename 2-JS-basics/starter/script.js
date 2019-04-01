0962b63a03b5c1dba3df87ce0253215e35285e1c
Dominic Davies <domdaviesit@gmail.com>
2019-04-01 22:30:03 +0100

    notes and exercises up to 2.9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,24 @@
-console.log("Helllo world");
+// Lecture 2.9 Variable Mutation and Type coercion
+
+
+
+// Lecture 2.8 - variables
+var firstName = 'John';
+// console.log(firstName);
+
+var lastName = 'Smith';
+
+var age = 28;
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+// Lecture 2.6
+// console.log("Helllo world");
