f7798409e2930b2ea8b43ca233a515e6e0b3b04d
Dominic Davies <domdaviesit@gmail.com>
2019-04-03 21:19:29 +0100

    add notes for lecture 9, add bootstrap jumbotron

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,24 +1,41 @@
 // Lecture 2.9 Variable Mutation and Type coercion
 
+var firstName = "Delboy";
+var age = 42;
 
+// console.log(firstName + ' ' + age);
 
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable Mutation 
+
+// age = 'twenty eight';
+// job = 'driver';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried)
+
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName);
 // Lecture 2.8 - variables
-var firstName = 'John';
-// console.log(firstName);
+// var firstName = 'John';
 
-var lastName = 'Smith';
+// var lastName = 'Smith';
 
-var age = 28;
+// var age = 28;
 
-var fullAge = true;
+// var fullAge = true;
 
-console.log(fullAge);
+// console.log(fullAge);
 
-var job;
-console.log(job);
+// var job;
+// console.log(job);
 
-job = 'teacher';
-console.log(job);
+// job = 'teacher';
+// console.log(job);
 
 // Lecture 2.6
 // console.log("Helllo world");
